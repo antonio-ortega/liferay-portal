61154a864345dd176b152086c96f76cd534adbfa
antonio-ortega <antonio.ortega@liferay.com>
2025-05-18 20:40:17 +0200

    Revert "LPD-51804 Add new system group for Calendar"
    This reverts commit 9aba859a6956786bcd8ce434ef063eed01b5ec6e.

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -452,7 +452,6 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 		if (className.equals(Group.class.getName())) {
 			if (!site && (liveGroupId == 0) &&
 				!(StringUtil.startsWith(groupKey, GroupConstants.APP) ||
-				  groupKey.equals(GroupConstants.CALENDAR) ||
 				  groupKey.equals(GroupConstants.CMS) ||
 				  groupKey.equals(GroupConstants.CONTROL_PANEL) ||
 				  groupKey.equals(GroupConstants.FORMS))) {
@@ -875,12 +874,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 				boolean site = true;
 				UnicodeProperties typeSettingsUnicodeProperties = null;
 
-				if (groupKey.equals(GroupConstants.CALENDAR)) {
-					type = GroupConstants.TYPE_SITE_PRIVATE;
-					friendlyURL = GroupConstants.CALENDAR_FRIENDLY_URL;
-					site = false;
-				}
-				else if (groupKey.equals(GroupConstants.CMS)) {
+				if (groupKey.equals(GroupConstants.CMS)) {
 					type = GroupConstants.TYPE_SITE_PRIVATE;
 					friendlyURL = GroupConstants.CMS_FRIENDLY_URL;
 					site = false;
