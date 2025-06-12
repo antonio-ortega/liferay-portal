513f4d6311ecf198519a598577b12c943b719842
antonio-ortega <antonio.ortega@liferay.com>
2025-06-12 09:29:37 +0200

    LPD-56873 WIP

---
+++ modules/test/playwright/tests/frontend-js-web/main/liferayGlobalObject.spec.ts
@@ -0,0 +1,65 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {expect, mergeTests} from '@playwright/test';
+
+import {apiHelpersTest} from '../../../fixtures/apiHelpersTest';
+import {instanceSettingsPagesTest} from '../../../fixtures/instanceSettingsPagesTest';
+import {featureFlagsTest} from '../../../fixtures/featureFlagsTest';
+import {isolatedSiteTest} from '../../../fixtures/isolatedSiteTest';
+import {loginTest} from '../../../fixtures/loginTest';
+import {productMenuPageTest} from '../../../fixtures/productMenuPageTest';
+import {siteSettingsPagesTest} from '../../../fixtures/siteSettingsPagesTest';
+import getRandomString from '../../../utils/getRandomString';
+import {performUserSwitch, userData} from '../../../utils/performLogin';
+
+export const test = mergeTests(
+	apiHelpersTest,
+	featureFlagsTest({
+		'LPS-178052': {enabled: true},
+	}),
+    instanceSettingsPagesTest,
+	isolatedSiteTest,
+	loginTest(),
+	productMenuPageTest,
+	siteSettingsPagesTest
+);
+
+test(
+	'CORS does not stop requests when CDN is enabled',
+	{tag: '@LPD-56873'},
+	async ({instanceSettingsPage, page}) => {
+		await test.step('Check crossorigin attribute is added when CDN is enabled', async () => {
+            await instanceSettingsPage.goToInstanceSetting(
+                'Instance Configuration',
+                'General'
+            );
+
+            await page.getByLabel('CDN Host HTTP', { exact: true }).fill('http://127.0.0.1');
+            
+            await page.getByLabel('CDN Host HTTPS').fill('https://127.0.0.1');
+            
+            await page.getByRole('button', { name: 'Save' }).click();
+
+            await expect(page.getByText('Main Configuration')).toBeVisible({timeout:5000});
+
+            await expect(
+                page.locator('script[src^="/o/frontend-js-web/Liferay.js"]')
+            ).toHaveAttribute('data-senna-track','permanent');
+		});
+
+        await test.step('Check crossorigin attribute is removed when CDN is disabled', async () => {
+            /*await page.getByLabel('CDN Host HTTP', { exact: true }).clear();
+            
+            await page.getByLabel('CDN Host HTTPS').clear();
+            
+            await page.getByRole('button', { name: 'Save' }).click();
+
+            await expect(
+                page.locator('script[src^="/o/frontend-js-web/Liferay.js"]')
+            ).not.toHaveAttribute('crossorigin','');*/
+        })
+	}
+);
