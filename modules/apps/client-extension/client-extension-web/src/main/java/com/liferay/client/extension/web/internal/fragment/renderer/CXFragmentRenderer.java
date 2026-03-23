a19f4ef42ec2c4bb973bc460806b13fbd7962e60
antonio-ortega <antonio.ortega@liferay.com>
2026-03-23 06:59:06 +0100

    LPD-80274 Create Fragment Renderer for Client Extensions

---
+++ modules/apps/client-extension/client-extension-web/src/main/java/com/liferay/client/extension/web/internal/fragment/renderer/CXFragmentRenderer.java
@@ -0,0 +1,110 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.client.extension.web.internal.fragment.renderer;
+
+import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.fragment.renderer.FragmentRendererContext;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Antonio Ortega
+ */
+
+@Component(service = FragmentRenderer.class)
+public class CXFragmentRenderer implements FragmentRenderer {
+
+	@Override
+	public String getCollectionKey() {
+		return "content-display";
+	}
+
+	@Override
+	public JSONObject getConfigurationJSONObject(
+		FragmentRendererContext fragmentRendererContext) {
+
+		return JSONUtil.put(
+			"fieldSets",
+			JSONUtil.putAll(
+				JSONUtil.put(
+					"fields",
+					JSONUtil.putAll(
+						JSONUtil.put(
+							"label", "client-extension"
+						).put(
+							"name", "itemSelector"
+						).put(
+							"type", "itemSelector"
+						)))));
+	}
+
+	@Override
+	public String getIcon() {
+		return "table";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return _language.get(locale, "client-extension");
+	}
+
+	@Override
+	public boolean isSelectable(HttpServletRequest httpServletRequest) {
+		return true;
+	}
+
+	@Override
+	public void render(
+			FragmentRendererContext fragmentRendererContext,
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		try {
+            PrintWriter printWriter = httpServletResponse.getWriter();
+
+            printWriter.write(
+            StringBundler.concat(
+                "<div class=\"portlet-msg-info\">",
+                _language.get(
+                    httpServletRequest, "select-a-client-extension"),
+                "</div>"));
+		}
+		catch (Exception exception) {
+			_log.error("Unable to render client extension", exception);
+
+			throw new IOException(exception);
+		}
+	}
+
+    private static final Log _log = LogFactoryUtil.getLog(
+        CXFragmentRenderer.class);
+
+    @Reference
+	private Language _language;
+}
