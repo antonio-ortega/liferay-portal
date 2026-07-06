1f04a7dd9c36558f0a93374be1ea77e1003e593a
antonio-ortega <antonio.ortega@liferay.com>
2026-07-06 21:50:33 +0200

    CX geolocation

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-audiences-geolocation/assets/geolocation.js
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-audiences-geolocation/assets/geolocation.js
@@ -3,25 +3,34 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export function timeOfDay() {
-	const hour = new Date().getHours();
+let reverseGeocodePromise;
 
-	if (hour < 5) {
-		return 'night';
-	}
-	else if (hour < 12) {
-		return 'morning';
-	}
-	else if (hour < 18) {
-		return 'afternoon';
-	}
-	else if (hour < 22) {
-		return 'evening';
+function getReverseGeocode() {
+	if (!reverseGeocodePromise) {
+		reverseGeocodePromise = new Promise((resolve, reject) => {
+			navigator.geolocation.getCurrentPosition(resolve, reject);
+		}).then(({coords}) =>
+			fetch(
+				`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coords.latitude}&longitude=${coords.longitude}`
+			).then((response) => response.json())
+		);
 	}
 
-	return 'night';
+	return reverseGeocodePromise;
 }
 
-export function touchDevice() {
-	return navigator.maxTouchPoints > 0;
+export async function country() {
+	const {countryName} = await getReverseGeocode();
+
+	console.log("countryName", countryName);
+
+	return countryName;
+}
+
+export async function city() {
+	const reverseGeocode = await getReverseGeocode();
+
+	console.log("city", reverseGeocode.city);
+
+	return reverseGeocode.city;
 }
